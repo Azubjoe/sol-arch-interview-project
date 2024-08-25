357c7bd2d7f5edf79f1522f5ac935838fe76f5f7
Sunny Okpala <azubijoe@gmail.com>
2024-08-25 17:57:20 +0100

    created vpc files

--- variables.tf
+++ variables.tf
@@ -13,3 +13,45 @@ variable "environment" {
     description = "environment"
     type        = string
 }
+
+# vpc variables
+variable "vpc_cidr" {
+    description = "vpc cidr block"
+    type        = string
+}
+
+# public subnet az1 variables
+variable "public_subnet_az1_cidr" {
+    description = "public subnet az1 cidr"
+    type        = string
+}
+
+# public subnet az2 variables
+variable "public_subnet_az2_cidr" {
+    description = "public subnet az2 cidr"
+    type        = string
+}
+
+# private app subnet az1 variables
+variable "private_app_subnet_az1_cidr" {
+    description = "private app subnet az1 cidr"
+    type        = string
+}
+
+# private app subnet az2 variables
+variable "private_app_subnet_az2_cidr" {
+    description = "private app subnet az2 cidr"
+    type        = string
+}
+
+# private data subnet az1 variables
+variable "private_data_subnet_az1_cidr" {
+    description = "private data subnet az1 cidr"
+    type        = string
+}
+
+ # private data subnet az2 variables
+variable "private_data_subnet_az2_cidr" {
+    description = "private data subnet az2 cidr"
+    type        = string
+}
